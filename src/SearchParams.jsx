bceaa60acf2ffb4cb3d5a222af8b8fb4097d7a9b
Anna Weakclaw <annas.wieclaw@gmail.com>
2023-08-28 23:00:07 +0100

    Add a call to 3rd part API - get an initial load of Pets with useEffect and then call it again on Submit

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Pet from "./Pet";
+const ANIMALS = ["brid", "dog", "cat", "rabbit", "reptile"];
 
 const SearchParams = () => {
   //   const location = "Seatlle, WA";
-  const [location, setLocation] = useState("Seattle, WA");
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const breeds = [];
+
+  useEffect(() => {
+    requestPets();
+  }, []);
+
+  async function requestPets() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
+
+    setPets(json.pets);
+  }
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -15,8 +39,51 @@ const SearchParams = () => {
             placeholder="Location"
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            id="animal"
+            value={animal}
+            placeholder="Animal"
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            id="breed"
+            disabled={breed.length === 0}
+            value={breed}
+            placeholder="Breed"
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
+
         <button>Submit</button>
       </form>
+
+      {pets.map((pet) => (
+        <Pet
+          name={pet.name}
+          animal={pet.animal}
+          breeed={pet.breed}
+          key={pet.id}
+        />
+      ))}
     </div>
   );
 };
